49fbdeb23c7954aadfa7f89ac9ee42bad900828b
Dendre1997@ <denyskichuk97@gmail.com>
2024-09-17 23:06:18 -0600

    auth fixed

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyArKGzh7jLICjCNSowZlMCZzknpHfwaOKU",
-  authDomain: "house-marketplace-app-3cbf5.firebaseapp.com",
-  projectId: "house-marketplace-app-3cbf5",
-  storageBucket: "house-marketplace-app-3cbf5.appspot.com",
-  messagingSenderId: "159617227624",
-  appId: "1:159617227624:web:fe75eba026ef0911f394dd"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
